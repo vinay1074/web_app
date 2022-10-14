1a3d5fe823c31783f1c6c55acf740264486c53d9
yuvaraj <yuvaraj.vinay74@gmail.com>
2022-10-14 10:52:19 +0000

    shared-library

---
+++ vars/mavn.groovy
@@ -0,0 +1,39 @@
+#!/usr/bin/env groovy
+
+def call(body){
+  def args = [
+    branch: '',
+    url: '',
+  ]
+body.resolveStrategy = Closure.DELIGATE_FIRST
+body.delegate = args
+body()
+echo "INFO: ${args.branch}"
+
+pipeline {
+    agent any
+    tools {
+        maven 'maven'
+        jdk 'jdk'
+            }
+    stages {
+        stage ('git checkout'){
+            steps{
+                checkout([
+                    $class: 'GitSCM',
+                      branches: [[name: args.branch]],
+                      userRemoteConfigs: [[url: args.url]]
+                ])
+            }
+        }
+        stage ('Build'){
+            steps{
+                sh 'mvn -Dmaven.test.failure.ignore=true install'
+            }
+        }
+
+    }
+}
+}
+
+
