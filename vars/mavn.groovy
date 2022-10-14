2a7fbd8fc91add8d4529c3244ebfc2dbb183d1fa
yuvaraj <yuvaraj.vinay74@gmail.com>
2022-10-14 11:38:28 +0000

    update

--- vars/mavn.groovy
+++ vars/mavn.groovy
@@ -4,18 +4,18 @@ class mavn{
     String url
     String branch
     
-    pipeline{
+    pipeline {
     agent any
-    stages{
-        stage ('GITSCM'){
-            steps{
+    stages {
+        stage ('GITSCM') {
+            steps {
                echo "${mvn.url}" 
                echo "${mvn.branch}"
               git 'url';        
              }
         }
-        stage('Build'){
-            steps{
+        stage ('Build') {
+            steps {
                 sh 'mvn clean package'
             }
         }
